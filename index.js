7b484cc725e30fbdcb80ee57b6dd9a01971b2623
RayceSL <raycel@spitwspots.com>
2024-07-18 10:58:50 -0800

    Makes two separate maps

--- index.js
+++ index.js
@@ -3,40 +3,38 @@ import { ValueNoise } from "value-noise-js";
 
 const seed = "1";
 const noise = new ValueNoise(seed);
-// Width of the map
-// Number of objects in an array
-const cols = 100;
+
 // Height of the map
 // Number of arrays in the map
-const rows = cols / 2;
-const zoom = 0.25;
+const rows = 10;
 
-let map = []; // An array of arrays
+// Width of the map
+// Number of objects in an array
+const cols = rows * 2;
 
-class Row {
+// ↗ Zoom out; ↘ Zoom in
+const zoom = 1;
+
+// ██   ██ ██    ██ ███    ███ ██ ██████  ██ ████████ ██    ██ 
+// ██   ██ ██    ██ ████  ████ ██ ██   ██ ██    ██     ██  ██  
+// ███████ ██    ██ ██ ████ ██ ██ ██   ██ ██    ██      ████   
+// ██   ██ ██    ██ ██  ██  ██ ██ ██   ██ ██    ██       ██    
+// ██   ██  ██████  ██      ██ ██ ██████  ██    ██       ██    
+
+// 2D array humidity map
+let hMap = [];
+
+// Humidity row
+class HRow {
     constructor() {
         this.row = [];
     }
     // Fill each row with as many noise values as there are columns
-    fill(y) {
+    fill(y, ascii) {
             for (let i = 0; i < cols; i++) {
-                this.row.push(perlin(i, y, false));
+                this.row.push(perlin(i, y, ascii));
         }
     }
-}
-
-// >0: Cold; >30: Warm; >60: Hot
-function temp(y) {
-    //                       ┌  Hypot.  ┐   ↓ Opposite
-    // let y = r * Math.sin( Θ * Math.PI / 180 );
-    // 
-    // ↓ Solved for Θ
-    // let Θ = ( 180 * Math.asin( y / r ) ) / Math.PI
-    //
-    // ↓ Adjusted for my purposes
-    let Θ = ( 180 * Math.asin( y / 4 ) ) / Math.PI
-
-    return Math.floor(Θ);
 }
 
 function perlin(x, y, ascii) {
@@ -92,12 +90,119 @@ function perlin(x, y, ascii) {
     }
 }
 
-// Fills the map with all the rows
+// Fills the humidity map with all the rows
 for (let i = 0; i < rows; i++) {
-    map[i] = new Row;
-    map[i].fill(i);
+    hMap[i] = new HRow;
+    hMap[i].fill(i, true);
 }
 
-for (let i = 0; i < rows; i++) {
-    console.log(map[i].row.join(""));
+// Outputs the humidity map to the console
+function logHMap() {
+    for (let i = 0; i < rows; i++) {
+        console.log(hMap[i].row.join(""));
+    }
 }
+
+// ████████ ███████ ███    ███ ██████     
+//    ██    ██      ████  ████ ██   ██    
+//    ██    █████   ██ ████ ██ ██████     
+//    ██    ██      ██  ██  ██ ██         
+//    ██    ███████ ██      ██ ██      ██ 
+
+let tMap = []; // Temperature map
+
+
+// Temperature row
+class TRow {
+    constructor() {
+        this.row = [];
+    }
+    // Fill each row with as many temperature values as there are columns
+    fill(Θ, ascii) {
+            for (let i = 0; i < cols; i++) {
+                this.row.push(temperature(Θ, ascii));
+        }
+    }
+}
+
+// Input degrees; 0 = 90°N; 90 = 0°N; 180 = -90°S
+// Returns the temperature as a percentage
+function temperature(Θ, ascii) {
+    let r = 1;
+    let y = Math.floor( ( r * Math.sin( Θ * Math.PI / 180 ) ) * 100 );
+
+    switch (ascii) {
+        case true:
+            if (y >= 90) {
+                return "@";
+            } else if (y >= 80) {
+                return "%";
+            } else if (y >= 70) {
+                return "#";
+            } else if (y >= 60) {
+                return "*";
+            } else if (y >= 50) {
+                return "+";
+            } else if (y >= 40) {
+                return "=";
+            } else if (y >= 30) {
+                return "-";
+            } else if (y >= 20) {
+                return ":";
+            } else if (y >= 10) {
+                return ".";
+            } else if (y >= 0) {
+                return " ";
+            }
+            break;
+        case false:
+            if (z >= 90) {
+                return "█";
+            } else if (y >= 80) {
+                return "█";
+            } else if (y >= 70) {
+                return "▓";
+            } else if (y >= 60) {
+                return "▓";
+            } else if (y >= 50) {
+                return "▒";
+            } else if (y >= 40) {
+                return "▒";
+            } else if (y >= 30) {
+                return "░";
+            } else if (y >= 20) {
+                return "░";
+            } else if (y >= 10) {
+                return " ";
+            } else if (y >= 0) {
+                return " ";
+            }
+            break;
+    }
+}
+
+// Fills the temperature map with all the rows
+for (let i = 0; i < rows; i++) {
+    let sectors = Math.floor( 180 / rows );
+
+    tMap[i] = new TRow;
+    tMap[i].fill(i * sectors, true);
+}
+
+// Outputs the temperature map to the console
+function logTMap() {
+    for (let i = 0; i < rows; i++) {
+        console.log(tMap[i].row.join(""));
+    }
+}
+
+//  ██████  ██    ██ ████████ ██████  ██    ██ ████████ 
+// ██    ██ ██    ██    ██    ██   ██ ██    ██    ██    
+// ██    ██ ██    ██    ██    ██████  ██    ██    ██    
+// ██    ██ ██    ██    ██    ██      ██    ██    ██    
+//  ██████   ██████     ██    ██       ██████     ██    
+
+console.log("Temperature:");
+logTMap();
+console.log("\nHumidity:")
+logHMap();
