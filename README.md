c618fe60e9c19327e5f10d1f91484578dbc0f8a0
RayceSL <raycel@spitwspots.com>
2024-07-18 09:43:32 -0800

    Finally makes a map

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Function of this program
 Generate an ASCII map of a randomly generated planet
+***
+        Cold    Warm    Hot
+Wet     Ice     Ocean   Ocean
+Humid   Taiga   Forest  Jungle
+Dry     Ice     Grass   Desert
+
+Tundra      /
+Jungle      ¶
+Taiga       ↑
+Forest      ♣
+Ice         *
+Grass       ,
+Desert      -
+Ocean       ≈
+Mountain    ▲
+
+Generate three 2D perlin noise --- one for humidity, one for mountains
+These two numbers define a vector --- the angle of the vector defines the biome
+The map will be 10 x 20 --- Temperature falls off from center
--- index.js
+++ index.js
@@ -1,97 +1,103 @@
-/*
-        Cold    Warm    Hot
-Wet     Ice     Ocean   Ocean
-Humid   Taiga   Forest  Jungle
-Dry     Ice     Grass   Desert
-
-Tundra      /
-Jungle      ¶
-Taiga       ↑
-Forest      ♣
-Ice         *
-Grass       ,
-Desert      -
-Ocean       ≈
-Mountain    ▲
-
-Generate three 2D perlin noise --- one for humidity, one for mountains
-These two numbers define a vector --- the angle of the vector defines the biome
-The map will be 10 x 20 --- Temperature falls off from center
-*/
-
 import chalk from "chalk";
 import { ValueNoise } from "value-noise-js";
 
-let map = [];
+const seed = "1";
+const noise = new ValueNoise(seed);
+// Width of the map
+// Number of objects in an array
+const cols = 100;
+// Height of the map
+// Number of arrays in the map
+const rows = cols / 2;
+const zoom = 0.25;
 
-// 0 ≥ x < 21 and 0 ≥ y < 11
-// >0: Wet; >33: Humid; >66: Dry
-function humidity(x, y) {
-    function humidityValue(x, y) {    const noise = new ValueNoise();
-        let humidity = noise.evalXY(x, y);
-        return Math.floor(humidity * 100);
+let map = []; // An array of arrays
+
+class Row {
+    constructor() {
+        this.row = [];
     }
-
-    if (humidityValue(x, y) > 66) {
-        return "Dry";
-    } else if (humidityValue(x, y) > 33) {
-        return "Humid";
-    } else {
-        return "Wet";
+    // Fill each row with as many noise values as there are columns
+    fill(y) {
+            for (let i = 0; i < cols; i++) {
+                this.row.push(perlin(i, y, false));
+        }
     }
 }
 
 // >0: Cold; >30: Warm; >60: Hot
 function temp(y) {
-    function tempValue(y) {
-        //                       ┌  Hypot.  ┐   ↓ Opposite
-        // let y = r * Math.sin( Θ * Math.PI / 180 );
-        // 
-        // ↓ Solved for Θ
-        // let Θ = ( 180 * Math.asin( y / r ) ) / Math.PI
-        //
-        // ↓ Adjusted for my purposes
-        let Θ = ( 180 * Math.asin( y / 9 ) ) / Math.PI
+    //                       ┌  Hypot.  ┐   ↓ Opposite
+    // let y = r * Math.sin( Θ * Math.PI / 180 );
+    // 
+    // ↓ Solved for Θ
+    // let Θ = ( 180 * Math.asin( y / r ) ) / Math.PI
+    //
+    // ↓ Adjusted for my purposes
+    let Θ = ( 180 * Math.asin( y / 4 ) ) / Math.PI
 
-        return Math.floor(Θ);
-    }
+    return Math.floor(Θ);
+}
 
-    if (tempValue(y) > 60) {
-        return "Hot";
-    } else if (tempValue(y) > 30) {
-        return "Warm";
-    } else {
-        return "Cold";
+function perlin(x, y, ascii) {
+    let z = Math.floor( noise.evalXY( x * zoom , y * zoom ) * 100 );
+    
+    switch (ascii) {
+        case true:
+            if (z >= 90) {
+                return "@";
+            } else if (z >= 80) {
+                return "%";
+            } else if (z >= 70) {
+                return "#";
+            } else if (z >= 60) {
+                return "*";
+            } else if (z >= 50) {
+                return "+";
+            } else if (z >= 40) {
+                return "=";
+            } else if (z >= 30) {
+                return "-";
+            } else if (z >= 20) {
+                return ":";
+            } else if (z >= 10) {
+                return ".";
+            } else if (z >= 0) {
+                return " ";
+            }
+            break;
+        case false:
+            if (z >= 90) {
+                return "█";
+            } else if (z >= 80) {
+                return "█";
+            } else if (z >= 70) {
+                return "▓";
+            } else if (z >= 60) {
+                return "▓";
+            } else if (z >= 50) {
+                return "▒";
+            } else if (z >= 40) {
+                return "▒";
+            } else if (z >= 30) {
+                return "░";
+            } else if (z >= 20) {
+                return "░";
+            } else if (z >= 10) {
+                return " ";
+            } else if (z >= 0) {
+                return " ";
+            }
+            break;
     }
 }
 
-// For now, temperatur is hot at the south pole
-function fillMap() {
-    for (let i = 0; i < 10; i++) {
-        map.push([
-            {"temp": temp(i), "humidity": humidity(i, i+0)},
-            {"temp": temp(i), "humidity": humidity(i, i+1)},
-            {"temp": temp(i), "humidity": humidity(i, i+2)},
-            {"temp": temp(i), "humidity": humidity(i, i+3)},
-            {"temp": temp(i), "humidity": humidity(i, i+4)},
-            {"temp": temp(i), "humidity": humidity(i, i+5)},
-            {"temp": temp(i), "humidity": humidity(i, i+6)},
-            {"temp": temp(i), "humidity": humidity(i, i+7)},
-            {"temp": temp(i), "humidity": humidity(i, i+8)},
-            {"temp": temp(i), "humidity": humidity(i, i+9)},
-            {"temp": temp(i), "humidity": humidity(i, i+10)},
-            {"temp": temp(i), "humidity": humidity(i, i+11)},
-            {"temp": temp(i), "humidity": humidity(i, i+12)},
-            {"temp": temp(i), "humidity": humidity(i, i+13)},
-            {"temp": temp(i), "humidity": humidity(i, i+14)},
-            {"temp": temp(i), "humidity": humidity(i, i+15)},
-            {"temp": temp(i), "humidity": humidity(i, i+16)},
-            {"temp": temp(i), "humidity": humidity(i, i+17)},
-            {"temp": temp(i), "humidity": humidity(i, i+18)},
-            {"temp": temp(i), "humidity": humidity(i, i+19)}
-        ]);
-    }
+// Fills the map with all the rows
+for (let i = 0; i < rows; i++) {
+    map[i] = new Row;
+    map[i].fill(i);
 }
 
-fillMap();
-console.log(map);
+for (let i = 0; i < rows; i++) {
+    console.log(map[i].row.join(""));
+}
